2071987a11e6450547a178da41afd5b2606763ce
viniosouza <vinicios2508@gmail.com>
2020-07-29 12:23:19 -0300

    App removido e home adicionado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import CadastroVideo from "./pages/Cadastro/Video";
+import CadastroCategoria from "./pages/Cadastro/Categoria";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+const Pagina404 = () => <div>Pagina 404</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
