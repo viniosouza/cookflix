8b1059e2cccbb0f6626c28f6b099fac18f4f8fb6
viniosouza <vinicios2508@gmail.com>
2020-08-06 19:07:28 -0300

    erros do eslint arrumado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Home from "./pages/Home";
-import VideoRegistration from "./pages/Register/Video";
-import CategoryRegistration from "./pages/Register/Category";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+/* eslint-disable linebreak-style */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import VideoRegistration from './pages/Register/Video';
+import CategoryRegistration from './pages/Register/Category';
 
 const Pagina404 = () => <div>Pagina 404</div>;
 
@@ -17,5 +18,5 @@ ReactDOM.render(
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
--- src/pages/Register/Category/constants.js
+++ src/pages/Register/Category/constants.js
@@ -1,4 +1,5 @@
-const hasCategories = (categories) =>
-  categories.lenght === 0 && categories.lenght;
+/* eslint-disable linebreak-style */
+const hasCategories = (categories) => categories.lenght === 0 && categories.lenght;
 
+// eslint-disable-next-line import/prefer-default-export
 export { hasCategories };
